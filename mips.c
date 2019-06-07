88e2b51f5ad6b56c2bd5f5088481e310285c1d6e
Lucas Félix <lucas.salaverry@edu.pucrs.br>
2019-06-07 17:07:36 -0300

    Começo da integração do bloco de controle com os métodos

--- mips.c
+++ mips.c
@@ -1,192 +1,20 @@
 #include <stdio.h>
 
-int pc;
-int A;
-int B;
-int saidaUla;
-int zero;
-int mux2(int a, int b, int controle);
-int mux3(int a, int b, int c, int controle);
-int mux4(int a, int b, int c, int d, int controle);
-int deslocaDois(int a);
-int ula(int a, int b, int operacao);
-int operacaoUla(int func, int controle);
-void atualizaPc(int value, int inc);
-int incPc(int zero, int PCEscControl, int PCEsc);
-void leituraRegistradores(int reg1, int reg2);
-void escritaRegistradores(int RegEsc, int reg, int dado);
-int leMemoria(int endereço, int lerMem);
-int extensaoDeSinal(int valor);
-int memoria[50];
-int registradores[32];
-
-int deslocaDois(int a){
-    return a << 2;
-}
-int mux2(int a, int b, int controle){
-    switch (controle)
-    {
-    case 0:
-        return a;
-        break;
-    case 1:
-        return b;
-        break;
-    default:
-        break;
-    }
-}
-
-int mux3(int a, int b, int c, int controle){
-    switch (controle)
-    {
-    case 0:
-        return a;
-        break;
-    case 1:
-        return b;
-        break;
-    case 2:
-        return c;
-        break;
-    default:
-        break;
-    }
-}
-
-int extensaoDeSinal(int valor){return valor;}
-
-int mux4(int a, int b, int c, int d, int controle){
-    switch (controle)
-    {
-    case 0:
-        return a;
-        break;
-    case 1:
-        return b;
-        break;
-    case 2:
-        return c;
-        break;
-    case 3:
-        return d;
-        break;
-    default:
-        break;
-    }
-}
-//FIXME: Conferir operações de sll e srl
-int operacaoUla(int func, int controle){
-    switch (controle)
-    {
-        case 0: // 00: lw, sw, addiu e lui a ula faz um add
-            return 2; // codigo do add
-        case 1: // 01: beq a ula faz um subb
-            return 6; // codigo do subb
-        case 2: // 10: operações do tipo-R
-            switch (func)
-            {
-                case 0: // 000000 sll VER QUAL OPERAÇÃO A ULA FAZ
-                    return 3; // ?????
-                case 2:// 000010 srl VER QUAL OPERAÇÃO A ULA FAZ
-                    return 4; // ??????
-                case 33: // 100001 addu 
-                    return 2; // a ula faz um add
-                case 36: // 100100 and
-                    return 0; // a ula faz um and
-                case 42: // 101010 slt
-                    return 7; // a ula faz um set on less then
-                default:
-                    break;
-            }
-        case 3: //11: ori a ula faz um or
-            return 1; // codigo do or
-        default:
-            break;
-    }
-}
-//FIXME: Conferir operações de sll e srl
-int ula(int a, int b, int operacao){
-    int resp;
-    switch (operacao)
-    {
-    case 0:
-        resp = a & b;
-    case 1:
-        resp = a | b;
-    case 2:
-        resp = a + b;
-    case 3:
-        resp = -1; //????
-    case 4:
-        resp =-1;//?????
-    case 6:
-        resp = a - b;
-    case 7:
-        resp = -1;// ver como fazer um set on less then
-    default:
-        break;
-    }
-    if(resp == 0)
-        zero = 1;
-    else
-        zero = 0;
-    return resp;
-}
-
-void atualizaPc(int value, int inc){
-    if(inc == 1){
-        pc = value;
-    }
-}
-
-int incPc(int zero, int PCEscControl, int PCEsc){
-    int temp = zero & PCEscControl;
-    return temp | PCEsc;
-}
-
-void leituraRegistradores(int reg1, int reg2){
-    A = registradores[reg1];
-    B = registradores[reg2];
-}
-
-void escritaRegistradores(int RegEsc, int reg, int dado){
-    if(RegEsc == 1){
-        registradores[reg] = dado;
-    }
-}
-//FIXME: implementar corretamente o caso em que a memoria não pode ser lida
-int memOp(int endereco, int dado, int EscMem, int LerMem){
-    if(EscMem == 1)
-        memoria[endereco] = dado;
-    else if(LerMem == 1)
-        return memoria[endereco];
-}
-
-//TODO: 
-//  Registradores intermediarios
-//  "Ligar os fios das funções"
-//  Controlador
-//  os "ciclos"
-//  Leitura de Arquivo
-
-
-
 //Estrutura do Bloco de BC
 struct Controle{
-    int PCEscCond;
-    int PCEsc; //Habilita a escrita no PC
-    int IouD; 
-    int LerMem; //Habilita Leitura da memória
-    int EscMem; //Habilita escrita em memória
+    int Branch; //Define se é uma instrução de branch.
+    int PCEsc; //Habilita a escrita no PC.
+    int IouD; //Define o endereço de memória trabalhado.
+    int LerMem; //Habilita Leitura da memória.
+    int EscMem; //Habilita escrita em memória.
     int MemParaReg;//Decide o que será escrito no registrador destino.
-    int IREsc; //Habilita registrador de instrução
-    int FontePC; //Decide o próximo PC (+4, desvio incondicional, desvio condicional)
-    int ULAOp; //Decide Operação da Ula
-    int ULAFonteB; //Decide o primeiro operando (B, 4, Imediato ou 26Bits Jump)
-    int ULAFonteA; //Decide o segundo operando (A ou PC)
-    int EscReg; //Habilita escrita nos registradores
-    int RegDst; //Decide qual o registrador destino
+    int IREsc; //Habilita registrador de instrução.
+    int FontePC; //Decide o próximo PC (+4, desvio incondicional, desvio condicional).
+    int ULAOp; //Decide Operação da Ula.
+    int ULAFonteB; //Decide o primeiro operando (B, 4, Imediato ou 26Bits Jump).
+    int ULAFonteA; //Decide o segundo operando (A ou PC).
+    int EscReg; //Habilita escrita nos registradores.
+    int RegDst; //Decide qual o registrador destino.
 
 }BC; //Bloco de Controle
 
@@ -206,15 +34,36 @@ enum Estados {
 
 enum Estados estadoAtual = Busca0;
 
-int opcode = 0;
+int opcode;
+int pc;
+int A;
+int B;
+int saidaUla;
+int zero;
+int mux2(int a, int b, int controle);
+int mux3(int a, int b, int c, int controle);
+int mux4(int a, int b, int c, int d, int controle);
+int deslocaDois(int a);
+int ula(int a, int b);
+int operacaoUla(int func, int controle);
+void atualizaPc(int value, int inc);
+int escritaPC(int zero);
+void leituraRegistradores(int reg1, int reg2);
+void escritaRegistradores(int RegEsc, int reg, int dado);
+int leMemoria(int endereço, int lerMem);
+int extensaoDeSinal(int valor);
+int memoria[50];
+int registradores[32];
 
 //Estabelece os sinais de acordo com o estado atual.
-void DadosBC(){
+void MaquinaEstados(){
 
     switch (estadoAtual)
     {
     case Busca0:
         //Busca da instrução -- 0 -> 1
+
+        //Define sinais de Controle
         BC.LerMem = 1;
         BC.ULAFonteA = 0;
         BC.IouD = 0;
@@ -223,6 +72,12 @@ void DadosBC(){
         BC.ULAOp = 0;
         BC.PCEsc = 1;
         BC.FontePC = 0;
+
+        //Ações
+        ula(pc,4); //PC = PC + 4
+        
+
+
         estadoAtual = Decodifica1;
         break;
 
@@ -304,7 +159,7 @@ void DadosBC(){
         BC.ULAFonteA = 1;
         BC.ULAFonteB = 0;
         BC.ULAOp = 1;
-        BC.PCEscCond = 1;
+        BC.Branch = 1;
         BC.PCEsc = 0;
         BC.FontePC = 1;
         estadoAtual = Busca0;
@@ -323,3 +178,168 @@ void DadosBC(){
 
 
 }
+
+
+
+int deslocaDois(int a)
+{
+    return a << 2;
+}
+int mux2(int a, int b, int controle)
+{
+    switch (controle)
+    {
+    case 0:
+        return a;
+        break;
+    case 1:
+        return b;
+        break;
+    default:
+        break;
+    }
+}
+
+int mux3(int a, int b, int c, int controle)
+{
+    switch (controle)
+    {
+    case 0:
+        return a;
+        break;
+    case 1:
+        return b;
+        break;
+    case 2:
+        return c;
+        break;
+    default:
+        break;
+    }
+}
+
+int extensaoDeSinal(int valor) { return valor; }
+
+int mux4(int a, int b, int c, int d, int controle)
+{
+    switch (controle)
+    {
+    case 0:
+        return a;
+        break;
+    case 1:
+        return b;
+        break;
+    case 2:
+        return c;
+        break;
+    case 3:
+        return d;
+        break;
+    default:
+        break;
+    }
+}
+//FIXME: Conferir operações de sll e srl
+int operacaoUla(int func, int controle)
+{
+    switch (controle)
+    {
+    case 0:       // 00: lw, sw, addiu e lui a ula faz um add
+        return 2; // codigo do add
+    case 1:       // 01: beq a ula faz um subb
+        return 6; // codigo do subb
+    case 2:       // 10: operações do tipo-R
+        switch (func)
+        {
+        case 0:       // 000000 sll VER QUAL OPERAÇÃO A ULA FAZ
+            return 3; // ?????
+        case 2:       // 000010 srl VER QUAL OPERAÇÃO A ULA FAZ
+            return 4; // ??????
+        case 33:      // 100001 addu
+            return 2; // a ula faz um add
+        case 36:      // 100100 and
+            return 0; // a ula faz um and
+        case 42:      // 101010 slt
+            return 7; // a ula faz um set on less then
+        default:
+            break;
+        }
+    case 3:       //11: ori a ula faz um or
+        return 1; // codigo do or
+    default:
+        break;
+    }
+}
+//FIXME: Conferir operações de sll e srl
+int ula(int a, int b)
+{
+    int resp;
+    switch (BC.ULAOp)
+    {
+    case 0:
+        resp = a & b;
+    case 1:
+        resp = a | b;
+    case 2:
+        resp = a + b;
+    case 3:
+        resp = -1; //????
+    case 4:
+        resp = -1; //?????
+    case 6:
+        resp = a - b;
+    case 7:
+        resp = -1; // ver como fazer um set on less then
+    default:
+        break;
+    }
+    if (resp == 0)
+        zero = 1;
+    else
+        zero = 0;
+    return resp;
+}
+
+void atualizaPc(int value, int inc)
+{
+    if (inc == 1)
+    {
+        pc = value;
+    }
+}
+
+int escritaPC(int zeroUla)
+{
+    int temp = zeroUla & BC.Branch;
+    return temp | BC.PCEsc;
+}
+
+void leituraRegistradores(int reg1, int reg2)
+{
+    A = registradores[reg1];
+    B = registradores[reg2];
+}
+
+void escritaRegistradores(int RegEsc, int reg, int dado)
+{
+    if (BC.EscReg == 1)
+    {
+        registradores[reg] = dado;
+    }
+}
+//FIXME: implementar corretamente o caso em que a memoria não pode ser lida
+int memOp(int endereco, int dado, int EscMem, int LerMem)
+{
+    if (EscMem == 1)
+        memoria[endereco] = dado;
+    else if (LerMem == 1)
+        return memoria[endereco];
+}
+
+//TODO:
+//  Registradores intermediarios
+//  "Ligar os fios das funções"
+//  Controlador
+//  os "ciclos"
+//  Leitura de Arquivo
